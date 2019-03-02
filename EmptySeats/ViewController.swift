29a3c3a4607a1608ac5d4654119fbbad72668161
siddhant-goel <siddhant@siddhants-macbook-pro.local>
2019-03-02 11:14:50 -0500

    Initial Commit

---
+++ EmptySeats/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EmptySeats
+//
+//  Created by Siddhant Goel on 3/2/19.
+//  Copyright © 2019 Siddhant Goel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
